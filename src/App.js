2d9f213ad118cbd5a7ab988e2a8abdca2d1e25ab
Channa Basava <Channa.Basava2@shell.com>
2021-06-04 22:37:12 +0530

    eee

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Channa
+          Learn React Channa First App
         </a>
       </header>
     </div>
